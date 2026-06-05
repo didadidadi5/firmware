fe6718f5e503f34933df561d2f9a2a3edea8f6fa
didadidadi5 <88706158+didadidadi5@users.noreply.github.com>
2026-06-06 06:41:38 +0800

    motors: link AN41908 with staging includes, libc libs (pthread/math), and use proper target flags

--- general/package/motors/motors.mk
+++ general/package/motors/motors.mk
@@ -13,37 +13,43 @@ MOTORS_LICENSE_FILES = LICENSE
 define MOTORS_BUILD_CMDS
 	# build camhi-motor if present
 	if [ -d $(@D)/camhi-motor ]; then \
-		(cd $(@D)/camhi-motor && $(TARGET_CC) -Os -s main.c -o camhi-motor); \
+		(cd $(@D)/camhi-motor && $(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_CPPFLAGS) -Os -s main.c -o camhi-motor $(TARGET_LDFLAGS) $(TARGET_LDLIBS)); \
 	fi
 	# build i2c-motor if present
 	if [ -d $(@D)/i2c-motor ]; then \
-		(cd $(@D)/i2c-motor && $(TARGET_CC) -Os -s main.c -o i2c-motor); \
+		(cd $(@D)/i2c-motor && $(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_CPPFLAGS) -Os -s main.c -o i2c-motor $(TARGET_LDFLAGS) $(TARGET_LDLIBS)); \
 	fi
 	# build xm-kmotor if present
 	if [ -d $(@D)/xm-kmotor ]; then \
-		(cd $(@D)/xm-kmotor && $(TARGET_CC) -Os -s main.c -o xm-kmotor); \
+		(cd $(@D)/xm-kmotor && $(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_CPPFLAGS) -Os -s main.c -o xm-kmotor $(TARGET_LDFLAGS) $(TARGET_LDLIBS)); \
 	fi
 	# build xm-uart if present
 	if [ -d $(@D)/xm-uart ]; then \
-		(cd $(@D)/xm-uart && $(TARGET_CC) -Os -s main.c -o xm-uart); \
+		(cd $(@D)/xm-uart && $(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_CPPFLAGS) -Os -s main.c -o xm-uart $(TARGET_LDFLAGS) $(TARGET_LDLIBS)); \
 	fi
 	# build ingenic-motor if present
 	if [ -d $(@D)/ingenic-motor ]; then \
-		(cd $(@D)/ingenic-motor && $(TARGET_CC) -Os -s main.c -o ingenic-motor); \
+		(cd $(@D)/ingenic-motor && $(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_CPPFLAGS) -Os -s main.c -o ingenic-motor $(TARGET_LDFLAGS) $(TARGET_LDLIBS)); \
 	fi
 	# build AN41908 variant (support both an41908 and an41908a directories and several source names)
 	if [ -d $(@D)/an41908 ]; then \
-		cd $(@D)/an41908; \
-		if [ -f main.c ]; then $(TARGET_CC) -Os -s main.c -o an41908; \
-		elif [ -f an41908.c ]; then $(TARGET_CC) -Os -s an41908.c -o an41908; \
-		elif [ -f an41908a.c ]; then $(TARGET_CC) -Os -s an41908a.c -o an41908; \
-		fi; \
+		(cd $(@D)/an41908 && \
+			if [ -f main.c ]; then \
+				$(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_CPPFLAGS) -I$(STAGING_DIR)/usr/include -Os -s main.c -o an41908 $(TARGET_LDFLAGS) $(TARGET_LDLIBS) -lm -lpthread; \
+			elif [ -f an41908.c ]; then \
+				$(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_CPPFLAGS) -I$(STAGING_DIR)/usr/include -Os -s an41908.c -o an41908 $(TARGET_LDFLAGS) $(TARGET_LDLIBS) -lm -lpthread; \
+			elif [ -f an41908a.c ]; then \
+				$(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_CPPFLAGS) -I$(STAGING_DIR)/usr/include -Os -s an41908a.c -o an41908 $(TARGET_LDFLAGS) $(TARGET_LDLIBS) -lm -lpthread; \
+			fi); \
 	elif [ -d $(@D)/an41908a ]; then \
-		cd $(@D)/an41908a; \
-		if [ -f main.c ]; then $(TARGET_CC) -Os -s main.c -o an41908; \
-		elif [ -f an41908a.c ]; then $(TARGET_CC) -Os -s an41908a.c -o an41908; \
-		elif [ -f an41908.c ]; then $(TARGET_CC) -Os -s an41908.c -o an41908; \
-		fi; \
+		(cd $(@D)/an41908a && \
+			if [ -f main.c ]; then \
+				$(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_CPPFLAGS) -I$(STAGING_DIR)/usr/include -Os -s main.c -o an41908 $(TARGET_LDFLAGS) $(TARGET_LDLIBS) -lm -lpthread; \
+			elif [ -f an41908a.c ]; then \
+				$(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_CPPFLAGS) -I$(STAGING_DIR)/usr/include -Os -s an41908a.c -o an41908 $(TARGET_LDFLAGS) $(TARGET_LDLIBS) -lm -lpthread; \
+			elif [ -f an41908.c ]; then \
+				$(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_CPPFLAGS) -I$(STAGING_DIR)/usr/include -Os -s an41908.c -o an41908 $(TARGET_LDFLAGS) $(TARGET_LDLIBS) -lm -lpthread; \
+			fi); \
 	fi
 endef
 
