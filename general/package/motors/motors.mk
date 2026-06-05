9ffb4eed9c52457c86cd9266b87a047eab16269c
didadidadi5 <88706158+didadidadi5@users.noreply.github.com>
2026-06-06 06:36:50 +0800

    motors: 修复编译错误，增强构建和安装健壮性（支持 an41908/an41908a 变体并处理缺失目录）

--- general/package/motors/motors.mk
+++ general/package/motors/motors.mk
@@ -11,21 +11,64 @@ MOTORS_LICENSE = MIT
 MOTORS_LICENSE_FILES = LICENSE
 
 define MOTORS_BUILD_CMDS
-	(cd $(@D)/camhi-motor; $(TARGET_CC) -Os -s main.c -o camhi-motor)
-	(cd $(@D)/i2c-motor; $(TARGET_CC) -Os -s main.c -o i2c-motor)
-	(cd $(@D)/xm-kmotor; $(TARGET_CC) -Os -s main.c -o xm-kmotor)
-	(cd $(@D)/xm-uart; $(TARGET_CC) -Os -s main.c -o xm-uart)
-	(cd $(@D)/ingenic-motor; $(TARGET_CC) -Os -s main.c -o ingenic-motor)
-	(cd $(@D)/an41908a; $(TARGET_CC) -Os -s an41908a.c -o an41908)
+	# build camhi-motor if present
+	if [ -d $(@D)/camhi-motor ]; then \
+		(cd $(@D)/camhi-motor && $(TARGET_CC) -Os -s main.c -o camhi-motor); \
+	fi
+	# build i2c-motor if present
+	if [ -d $(@D)/i2c-motor ]; then \
+		(cd $(@D)/i2c-motor && $(TARGET_CC) -Os -s main.c -o i2c-motor); \
+	fi
+	# build xm-kmotor if present
+	if [ -d $(@D)/xm-kmotor ]; then \
+		(cd $(@D)/xm-kmotor && $(TARGET_CC) -Os -s main.c -o xm-kmotor); \
+	fi
+	# build xm-uart if present
+	if [ -d $(@D)/xm-uart ]; then \
+		(cd $(@D)/xm-uart && $(TARGET_CC) -Os -s main.c -o xm-uart); \
+	fi
+	# build ingenic-motor if present
+	if [ -d $(@D)/ingenic-motor ]; then \
+		(cd $(@D)/ingenic-motor && $(TARGET_CC) -Os -s main.c -o ingenic-motor); \
+	fi
+	# build AN41908 variant (support both an41908 and an41908a directories and several source names)
+	if [ -d $(@D)/an41908 ]; then \
+		cd $(@D)/an41908; \
+		if [ -f main.c ]; then $(TARGET_CC) -Os -s main.c -o an41908; \
+		elif [ -f an41908.c ]; then $(TARGET_CC) -Os -s an41908.c -o an41908; \
+		elif [ -f an41908a.c ]; then $(TARGET_CC) -Os -s an41908a.c -o an41908; \
+		fi; \
+	elif [ -d $(@D)/an41908a ]; then \
+		cd $(@D)/an41908a; \
+		if [ -f main.c ]; then $(TARGET_CC) -Os -s main.c -o an41908; \
+		elif [ -f an41908a.c ]; then $(TARGET_CC) -Os -s an41908a.c -o an41908; \
+		elif [ -f an41908.c ]; then $(TARGET_CC) -Os -s an41908.c -o an41908; \
+		fi; \
+	fi
 endef
 
 define MOTORS_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 -D $(@D)/camhi-motor/camhi-motor $(TARGET_DIR)/usr/bin/camhi-motor
-	$(INSTALL) -m 0755 -D $(@D)/i2c-motor/i2c-motor $(TARGET_DIR)/usr/bin/i2c-motor
-	$(INSTALL) -m 0755 -D $(@D)/xm-kmotor/xm-kmotor $(TARGET_DIR)/usr/bin/xm-kmotor
-	$(INSTALL) -m 0755 -D $(@D)/xm-uart/xm-uart $(TARGET_DIR)/usr/bin/xm-uart
-	$(INSTALL) -m 0755 -D $(@D)/ingenic-motor/ingenic-motor $(TARGET_DIR)/usr/bin/ingenic-motor
-	$(INSTALL) -m 0755 -D $(@D)/an41908a/an41908 $(TARGET_DIR)/usr/bin/an41908
+	if [ -f $(@D)/camhi-motor/camhi-motor ]; then \
+		$(INSTALL) -m 0755 -D $(@D)/camhi-motor/camhi-motor $(TARGET_DIR)/usr/bin/camhi-motor; \
+	fi
+	if [ -f $(@D)/i2c-motor/i2c-motor ]; then \
+		$(INSTALL) -m 0755 -D $(@D)/i2c-motor/i2c-motor $(TARGET_DIR)/usr/bin/i2c-motor; \
+	fi
+	if [ -f $(@D)/xm-kmotor/xm-kmotor ]; then \
+		$(INSTALL) -m 0755 -D $(@D)/xm-kmotor/xm-kmotor $(TARGET_DIR)/usr/bin/xm-kmotor; \
+	fi
+	if [ -f $(@D)/xm-uart/xm-uart ]; then \
+		$(INSTALL) -m 0755 -D $(@D)/xm-uart/xm-uart $(TARGET_DIR)/usr/bin/xm-uart; \
+	fi
+	if [ -f $(@D)/ingenic-motor/ingenic-motor ]; then \
+		$(INSTALL) -m 0755 -D $(@D)/ingenic-motor/ingenic-motor $(TARGET_DIR)/usr/bin/ingenic-motor; \
+	fi
+	# install AN41908 if built in any of the supported dirs
+	if [ -f $(@D)/an41908/an41908 ]; then \
+		$(INSTALL) -m 0755 -D $(@D)/an41908/an41908 $(TARGET_DIR)/usr/bin/an41908; \
+	elif [ -f $(@D)/an41908a/an41908 ]; then \
+		$(INSTALL) -m 0755 -D $(@D)/an41908a/an41908 $(TARGET_DIR)/usr/bin/an41908; \
+	fi
 endef
 
 $(eval $(generic-package))
