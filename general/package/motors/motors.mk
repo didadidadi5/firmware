56fe1a7c9287c0ebe591ccc020266c1ab0e485f7
didadidadi5 <88706158+didadidadi5@users.noreply.github.com>
2026-06-06 23:45:08 +0800

    Fix shell continuation and missing fi in MOTORS_BUILD_CMDS in motors.mk

--- general/package/motors/motors.mk
+++ general/package/motors/motors.mk
@@ -13,7 +13,7 @@ MOTORS_LICENSE_FILES = LICENSE
 define MOTORS_BUILD_CMDS
 	# helper: pick include dir for HiSilicon headers (try toolchain SDK, then staging, then target)
 	# Note: output/host/sdk/include is where CI/toolchain unpacks SDK headers
-	INCLUDE_FLAGS=""
+	INCLUDE_FLAGS=""; \
 	if [ -f "/home/runner/work/firmware/firmware/output/host/sdk/include/hi_type.h" ]; then \
 		INCLUDE_FLAGS="-I/home/runner/work/firmware/firmware/output/host/sdk/include"; \
 	elif [ -f "$(STAGING_DIR)/usr/include/hi_type.h" ]; then \
@@ -52,10 +52,10 @@ define MOTORS_BUILD_CMDS
 			fi); \
 	elif [ -d $(@D)/an41908a ]; then \
 		(cd $(@D)/an41908a && \
-	  if [ -f an41908a.c ]; then \
-			$(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_CPPFLAGS) $$INCLUDE_FLAGS -Os -s an41908a.c -o an41908 $(TARGET_LDFLAGS) $(TARGET_LDLIBS) -lpthread -lm; \
-	  fi;\
- fi;\
+			if [ -f an41908a.c ]; then \
+				$(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_CPPFLAGS) $$INCLUDE_FLAGS -Os -s an41908a.c -o an41908 $(TARGET_LDFLAGS) $(TARGET_LDLIBS) -lpthread -lm; \
+			fi); \
+	fi; \
 endef
 
 define MOTORS_INSTALL_TARGET_CMDS
@@ -73,13 +73,13 @@ define MOTORS_INSTALL_TARGET_CMDS
 	fi
 	if [ -f $(@D)/ingenic-motor/ingenic-motor ]; then \
 		$(INSTALL) -m 0755 -D $(@D)/ingenic-motor/ingenic-motor $(TARGET_DIR)/usr/bin/ingenic-motor; \
-	fi 
+	fi
 	# install AN41908 if built in any of the supported dirs
 	if [ -f $(@D)/an41908/an41908 ]; then \
 		$(INSTALL) -m 0755 -D $(@D)/an41908/an41908 $(TARGET_DIR)/usr/bin/an41908; \
 	elif [ -f $(@D)/an41908a/an41908 ]; then \
 		$(INSTALL) -m 0755 -D $(@D)/an41908a/an41908 $(TARGET_DIR)/usr/bin/an41908; \
-	fi ;\
+	fi
 endef
 
 $(eval $(generic-package))
